3b8422bce1c1e2534b9b6dd3a82238ec1735e14b
robertogomesbr <robertogomes0277@gmail.com>
2025-12-15 04:41:51 -0300

    Configurações iniciais das legendas

--- private/js/legendaManual.js
+++ private/js/legendaManual.js
@@ -1,4 +1,7 @@
 const container = document.getElementById('playerContainer');
+const btnAdd = document.getElementById('btnAdd');
+const legendaInput = document.getElementById('legendaInput');
+const legendaDisplay = document.getElementById('legendaDisplay');
 
 const params = new URLSearchParams(window.location.search);
 const file = params.get('file');
@@ -12,13 +15,19 @@ const ext = file.split('.').pop().toLowerCase();
 
 const src = `/uploads/${file}`;
 
-if (['mp4','webm','ogg'].includes(ext)) {
+let media;
+
+if (['mp4','webm'].includes(ext)) {
 
     container.innerHTML = `<video controls width="800"><source src="${src}">Seu navegador não suporta vídeo.</video>`;
+
+    media = container.querySelector('video');
 
 } else if (['mp3','wav','ogg'].includes(ext)) {
 
     container.innerHTML = `<audio controls><source src="${src}">Seu navegador não suporte aúdio.</audio>`;
+
+    media = container.querySelector('audio');
 
 } else {
 
@@ -26,3 +35,25 @@ if (['mp4','webm','ogg'].includes(ext)) {
     window.location.href = '/private/home.html';
 
 }
+
+const legendas = [];
+
+btnAdd.addEventListener('click', () => {
+    if(!legendaInput.value.trim()) return; 
+
+    legendas.push({
+        texto: legendaInput.value,
+        tempo: media.currentTime
+    });
+
+    legendaInput.value = '';
+    console.log(legendas);
+});
+
+media.addEventListener('timeupdate', () => {
+    const atual = legendas.find((l) =>
+        media.currentTime >= l.tempo && media.currentTime < l.tempo + 2
+    );
+
+    legendaDisplay.innerText = atual ? atual.texto : '';
+});
